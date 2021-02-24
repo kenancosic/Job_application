8466652198d10492b2f20ea4118fb8d2f911f3e2
kenancosic <cosic.kenan@outlook.com>
2021-02-24 08:57:22 +0100

    first commit

---
+++ src/main/java/com/springproject/job_application/JobApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.job_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+
+}
